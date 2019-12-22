74cdbcee9acace564002b033599c183b0655d216
ivolnoboi <irinavolnoboy@gmail.com>
2019-12-22 15:38:28 +0300

    Added tests for Breed

--- test/models/breed_test.rb
+++ test/models/breed_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class BreedTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'breed empty pet_type' do
+    assert_equal Breed.create(name: nil).valid?, false
+  end
+
+  test 'breed not empty pet_type' do
+    assert_equal Breed.create(name: 'хаски').valid?, true
+  end
+
+  test 'breed unique pet_type' do
+    assert_equal Breed.create(name: 'хаски').valid?, true
+    assert_equal Breed.create(name: 'хаски').valid?, false
+  end
 end
