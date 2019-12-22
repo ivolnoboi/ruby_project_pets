60202489a7af68b454236f7edcd64d88461aea46
ivolnoboi <irinavolnoboy@gmail.com>
2019-12-22 15:34:43 +0300

    Added tests for TypePet

--- test/models/type_pet_test.rb
+++ test/models/type_pet_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class TypePetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'type_pet empty pet_type' do
+    assert_equal TypePet.create(pet_type: nil).valid?, false
+  end
+
+  test 'type_pet not empty pet_type' do
+    assert_equal TypePet.create(pet_type: 'cat').valid?, true
+  end
+
+  test 'type_pet unique pet_type' do
+    assert_equal TypePet.create(pet_type: 'cat').valid?, true
+    assert_equal TypePet.create(pet_type: 'cat').valid?, false
+  end
 end
