c600adc1e902b8dc7709a3bc98174f391e0a59d9
ivolnoboi <irinavolnoboy@gmail.com>
2019-12-22 19:54:00 +0300

    Added tests for Report

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'test report' do
+    re = ReportType.create(name: 'name')
+    assert_equal Report.create(title: 'title', content: 'content', report_type: re).valid?, true
+  end
+
+  test 'test report empty title' do
+    re = ReportType.create(name: 'name')
+    assert_equal Report.create(title: nil, content: 'content', report_type: re).valid?, false
+  end
+
+  test 'test report empty content' do
+    re = ReportType.create(name: 'name')
+    assert_equal Report.create(title: 'title', content: nil, report_type: re).valid?, true
+  end
+
+  test 'test report empty report_type' do
+    assert_equal Report.create(title: 'title', content: 'content', report_type: nil).valid?, false
+  end
 end
