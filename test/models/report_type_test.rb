961fefda19dd7437ac6051a8700c86c566d47e23
ivolnoboi <irinavolnoboy@gmail.com>
2019-12-22 16:46:06 +0300

    Added test for ReportType

--- test/models/report_type_test.rb
+++ test/models/report_type_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class ReportTypeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'test report_type' do
+    assert_equal ReportType.create(name: 'name').valid?, true
+  end
+
+  test 'test report_type empty name' do
+    assert_equal ReportType.create(name: nil).valid?, false
+  end
+
+  test 'test report_type unique name' do
+    assert_equal ReportType.create(name: 'name').valid?, true
+    assert_equal ReportType.create(name: 'name').valid?, false
+  end
 end
