fb69f15534009b5eeeeaf34363417035e46c18df
ivolnoboi <irinavolnoboy@gmail.com>
2019-12-22 15:46:18 +0300

    Added tests for location

--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class LocationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'test location' do
+    assert_equal Location.create(city: 'city', district: 'district',
+                                 street: 'street', house: '38', flat: '12').valid?, true
+  end
+
+  test 'test location empty city' do
+    assert_equal Location.create(city: nil, district: 'district',
+                                 street: 'street', house: '38', flat: '12').valid?, false
+  end
+
+  test 'test location empty district' do
+    assert_equal Location.create(city: 'city', district: nil,
+                                 street: 'street', house: '38', flat: '12').valid?, true
+  end
+
+  test 'test location empty street' do
+    assert_equal Location.create(city: 'city', district: 'district',
+                                 street: nil, house: '38', flat: '12').valid?, false
+  end
+
+  test 'test location empty house' do
+    assert_equal Location.create(city: 'city', district: 'district',
+                                 street: 'street', house: nil, flat: '12').valid?, false
+  end
+
+  test 'test location empty flat' do
+    assert_equal Location.create(city: 'city', district: 'district',
+                                 street: 'street', house: '38', flat: nil).valid?, true
+  end
 end
