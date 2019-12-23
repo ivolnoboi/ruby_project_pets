efb32b90c1644c382424278ba8689ac887d703a4
ivolnoboi <irinavolnoboy@gmail.com>
2019-12-23 20:48:10 +0300

    Added about_us

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'home#index'
+
+  get '/pets', to: 'pets#index'
+  get '/about_us', to: 'about_us#index'
 end
